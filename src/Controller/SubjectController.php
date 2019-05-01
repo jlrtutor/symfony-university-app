3cf17f92631773f69a2e05c588eb467ee3e7e39d
Juan Luis Ramírez Tutor <juanluis.ramirez.tutor@gmail.com>
2019-05-01 20:22:48 +0200

    Added Toastr alert component and ajax call to change subject status

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -4,10 +4,36 @@ namespace App\Controller;
 
 use App\Entity\Subject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class SubjectController extends AbstractController
 {
+    public function ajaxChangeStatus(Request $request, $id)
+    {
+        $manager = $this->getDoctrine()->getManager();        
+        $subject = $manager->getRepository(Subject::class)->find($id);
+
+        $subject->setIsActive( $subject->getIsActive()?false:true );
+        $manager->flush();
+
+        //Ajax Calls Only
+        if (!$request->isXmlHttpRequest()) {
+            return new JsonResponse(array(
+                'status' => 'Error',
+                'message' => 'Error'),
+            400);
+        }
+
+        // Send all this back to client
+        return new JsonResponse(array(
+            'status' => 'OK',
+            'message' => '1'),
+        200);
+    }
+
 
     public function list()
     {
